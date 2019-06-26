c8c26e257c2887e3a7b42ee314855fb7cc5ad6b3
Yogender Jangir <201651054@iiitvadodara.ac.in>
2019-06-26 11:20:05 +0530

    Final Customize Navbar

--- e-cart-react/src/components/Navbar.js
+++ e-cart-react/src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { ButtonContainer } from "./Button";
+import styled from "styled-components";
 import logo from "../logo.svg";
 
 export class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm  navbar-dark px-sm-5">
         {/*
             https://www.iconfinder.com/icons/1243689/call_phone_icon
             Creative Commons (Attribution 3.0 Unported);
@@ -17,19 +19,29 @@ export class Navbar extends Component {
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
             <Link to="/" className="nav-link">
-              Products
+              products
             </Link>
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus" />
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus" />
+            </span>
             my cart
-          </button>
+          </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
 
+const NavWrapper = styled.nav`
+  background: var(--mainBlue);
+  .nav-link {
+    color: var(--mainWhite) !important;
+    font-size: 1.3rem;
+    text-transform: capitalize;
+  }
+`;
 export default Navbar;
